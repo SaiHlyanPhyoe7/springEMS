d515c9d69e0213b7922e05d0d2931d6eac0a28c3
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-06 22:59:24 +0630

    initial commit

---
+++ src/test/java/net/javaguides/emsbackend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
